73ab91b1f4b4127f02f4aeaec6a11de7b13effd6
Valimp <valentinchauvet.contact@gmail.com>
2024-03-11 10:34:02 +0100

    feat: init V1

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 # ShareLoc
-
-## What is ShareLoc ?
-ShareLoc is a full stack website to make location in CtoC.
-
-## Who works on this project ? 
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='bg-orange-300'>HomePage</div>
+  )
+}
+
+export default HomePage
