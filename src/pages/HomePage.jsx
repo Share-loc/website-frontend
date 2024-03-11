8103cee2c2f86d0abed47f347d56cb1ac35f3048
Valimp <valentinchauvet.contact@gmail.com>
2024-03-11 12:14:51 +0100

    fix: migrate to typescript

--- src/pages/HomePage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HomePage = () => {
-  return (
-    <div className='bg-orange-300'>HomePage</div>
-  )
-}
-
-export default HomePage
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+const HomePage = () => {
+  return (
+    <div className="bg-orange-400">HomePage</div>
+  )
+}
+
+export default HomePage
