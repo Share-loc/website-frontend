2f4a273f12ba4bc38263924286cdcde3e014cd21
Valimp <valentinchauvet.contact@gmail.com>
2024-05-02 11:38:46 +0200

    fix: responsive navbar

--- src/components/AddAdButton.tsx
+++ src/components/AddAdButton.tsx
@@ -6,7 +6,7 @@ const IconButton = () => {
   return (
     <Link to="/adpage" className="h-10 rounded-lg flex justify-between items-center bg-orange-100 hover:bg-orange-200 text-white hover:text-gray p-3 gap-3">
         <RiMapPinAddFill size={20} />
-        <p className="font-semibold">
+        <p className="font-semibold lg:block hidden">
             Déposer une annonce
         </p>
     </Link>
--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,20 +1,22 @@
 import AddAdButton from "./AddAdButton"
 import SearchBar from "./SearchBar/SearchBar"
 import Logo from "/logo.svg"
+import NavButton from "./NavButton"
+import { FaUser } from "react-icons/fa"
 
 const AppBar = () => {
   return (
     <header className="bg-white flex justify-around items-center h-16 shadow-lg">
-        <h1>
-            <img src={Logo} alt="logo" className="h-12" />
-        </h1>
+        <a href="/">
+            <img src={Logo} alt="logo" className="lg:h-10 h-7" />
+        </a>
         <AddAdButton />
         <SearchBar />
-        <nav>
+        <nav className="">
             <ul className="flex gap-4 text-blue">
-                <li><a href="/">Mes favoris</a></li>
-                <li><a href="/">Messagerie</a></li>
-                <li><a href="/">Mon compte</a></li>
+                <NavButton href="/login" text="connexion">
+                  <FaUser />
+                </NavButton>
             </ul>
         </nav>
     </header>
