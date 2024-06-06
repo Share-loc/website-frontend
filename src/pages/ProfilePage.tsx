bfa2944c398c8c25b00f59ea85868f696ace8267
Valimp <valentinchauvet.contact@gmail.com>
2024-06-06 15:32:35 +0200

    feat: handleshow inputs to update profile

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -17,6 +17,21 @@ const ProfilePage = () => {
     avatar: null,
   })
 
+  const [showInputs, setShowInputs] = useState({
+    avatar: false,
+    email: false,
+    username: false,
+  })
+
+  // handle show inputs take input name as argument and return the showInputs state with the input name set to opposite of current value
+  const handleShowInputs = (input: string) => {
+    console.log(showInputs);
+    setShowInputs({
+      ...showInputs,
+      [input]: !showInputs[input]
+    })
+  }
+
   useEffect(() => {
     // get user id in local storage
     const userid = getUserid();
@@ -46,14 +61,26 @@ const ProfilePage = () => {
       <h1 className="text-3xl font-bold mb-10">Mon profil</h1>
       <div className="flex flex-col items-start gap-5">
         {user.avatar && <img src={user.avatar} alt="avatar" className="w-32 h-32 rounded-full" />}
-        <p className="text-lg font-bold">
-          <span className='text-blue mr-10'>Nom d'utilisateur :</span>
-          {user.username}
-        </p>
-        <p className="text-lg font-bold">
-          <span className='text-blue mr-10'>Email :</span>
-          {user.email}
-        </p>
+        <div className="flex gap-5">
+          <p className="text-lg font-bold">
+            <span className='text-blue mr-10'>Nom d'utilisateur :</span>
+            {user.username}
+          </p>
+          <input type="text" className={`border-b-2 border-blue ${showInputs.username ? 'block' : 'hidden'}`} />
+          <button onClick={() => handleShowInputs('username')} className="text-blue">
+            {showInputs.username ? 'Annuler' : 'Modifier'}
+          </button>
+        </div>
+        <div className="flex gap-5">
+          <p className="text-lg font-bold">
+            <span className='text-blue mr-10'>Email :</span>
+            {user.email}
+          </p>
+          <input type="text" className={`border-b-2 border-blue ${showInputs.email ? 'block' : 'hidden'}`} />
+          <button onClick={() => handleShowInputs('email')} className="text-blue">
+            {showInputs.email ? 'Annuler' : 'Modifier'}
+          </button>
+        </div>
       </div>
     </div>
   )
