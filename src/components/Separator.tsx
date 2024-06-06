12a02ccd8d63d9bd425136a704a3fc9d5a8871e3
Valimp <valentinchauvet.contact@gmail.com>
2024-06-06 12:20:11 +0200

    feat: Footer

---
+++ src/components/Separator.tsx
@@ -0,0 +1,7 @@
+const Separator = () => {
+  return (
+    <span className={`h-[1px] bg-blue`}></span>
+  )
+}
+
+export default Separator
--- src/components/layouts/AppBarLayout.tsx
+++ src/components/layouts/AppBarLayout.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import AppBar from '../AppBar'
+import Footer from '../Footer'
 
 const AppBarLayout = ({children}: {children: React.ReactNode}) => {
   return (
     <>
         <AppBar />
         {children}
+        <Footer />
     </>
   )
 }
