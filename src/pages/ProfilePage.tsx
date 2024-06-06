b209a33fa15b7e9d7eb269d3f4d17d375a05a508
Valimp <valentinchauvet.contact@gmail.com>
2024-06-06 10:26:10 +0200

    feat: dynamic profile page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,6 +1,61 @@
+import { useEffect, useState } from 'react'
+import { getUserid, getToken } from '../const/func'
+import axios from 'axios'
+
+interface User {
+  email: string;
+  username: string;
+  avatar: string | null;
+
+}
+
 const ProfilePage = () => {
+
+  const [user, setUser] = useState({
+    email: '',
+    username: '',
+    avatar: null,
+  })
+
+  useEffect(() => {
+    // get user id in local storage
+    const userid = getUserid();
+    const token = getToken();
+    if (!userid || !token) {
+      // user is already redirect to login page if not logged in
+      return;
+    }
+    // fetch user data with token from local storage
+    axios.get(`${import.meta.env.VITE_API_URL}/users/${userid}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    .then(response => {
+      setUser(
+        {
+          email: response.data.email,
+          username: response.data.username,
+          avatar: response.data.avatar,
+        }
+      )
+    })
+
+  }, [getUserid(), getToken()])
+
   return (
-    <div>ProfilePage</div>
+    <div>
+      <h1 className="text-3xl font-bold mb-10">Mon profil</h1>
+      <div className="flex flex-col items-start gap-5">
+        {user.avatar && <img src={user.avatar} alt="avatar" className="w-32 h-32 rounded-full" />}
+        <p className="text-lg font-bold">
+          <span className='text-blue mr-10'>Nom d'utilisateur :</span>
+          {user.username}
+        </p>
+        <p className="text-lg font-bold">
+          <span className='text-blue mr-10'>Email :</span>
+          {user.email}
+        </p>
+      </div>
+    </div>
   )
 }
 
