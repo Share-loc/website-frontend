8fd35be15a144e7dba6008e3dec4a10a5048d589
Romain Ramanzin <romain.ramanzin@gmail.com>
2024-06-30 23:49:07 +0200

    feat - edit/delete for users

--- src/components/layouts/AdminLayout.tsx
+++ src/components/layouts/AdminLayout.tsx
@@ -6,7 +6,7 @@ const AdminLayout = () => {
         <>
             <div className="flex flex-1 h-screen bg-gray-50">
                 <AdminMenu />
-                <div className="flex flex-col flex-1">
+                <div className="flex flex-col flex-1 overflow-auto">
                     <main>
                     <div className="py-6">
                         <div className="px-4 mx-auto max-w-7xl sm:px-6 md:px-8">
