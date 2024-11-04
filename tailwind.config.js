b320498e343b4f8c0e9c3c81e3aa1ce25dbc34ca
Valimp <valentinchauvet.contact@gmail.com>
2024-11-04 11:53:10 +0100

    feat: instal shadnui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,75 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        "orange": {
-          "100": "#FEB24D",
-          "200": "#FEA634",
-        },
-        "blue": "#22AFAF",
-        "blue-dark": "#1D7A7A",
-        "gray": "#ECECEC",
-        "gray-dark": "#BDBDBD",
-        "black": "#141414",
-        "white": "#F3F3F3"
-      },
-    },
+  	extend: {
+  		colors: {
+  			'orange': {
+  				'100': '#FEB24D',
+  				'200': '#FEA634'
+  			},
+  			'blue': '#22AFAF',
+  			'blue-dark': '#1D7A7A',
+  			'gray': '#ECECEC',
+  			'gray-dark': '#BDBDBD',
+  			'black': '#141414',
+  			'white': '#F3F3F3',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
   plugins: [
     // ...
     require('@tailwindcss/forms'),
-  ],
+      require("tailwindcss-animate")
+],
 }
 
