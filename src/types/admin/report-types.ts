ca8ea1dcadf96793994c449656fdc70088678c6e
Romain Ramanzin <romain.ramanzin@gmail.com>
2025-02-28 02:22:27 +0100

    feat - redesign reports list

---
+++ src/types/admin/report-types.ts
@@ -0,0 +1,28 @@
+export interface Report {
+  id: number;
+  reporter: {
+    id: number;
+    email: string;
+    username: string;
+  };
+  reporter_message: string;
+  created_at: string;
+  status: string;
+  message?: {
+    id: number;
+    content: string;
+  };
+  review?: {
+    id: number;
+    content: string;
+  };
+  item?: {
+    id: number;
+    title: string;
+  };
+  user?: {
+    id: number;
+    email: string;
+    username: string;
+  };
+}
