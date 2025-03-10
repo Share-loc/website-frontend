f29940464fb29238ff9b4fa12ff7493b08f51cda
sinan <sinan.yaz@hotmail.com>
2025-03-10 16:20:41 +0100

    correction card item / les favoris / correction filtre et favoris items

--- src/pages/FavorisPage.tsx
+++ src/pages/FavorisPage.tsx
@@ -3,44 +3,30 @@ import AllCardsItems from "../components/ItemsComponents/allCardsItems";
 
 const FavorisPage = () => {
     const [items, setItems] = useState<any>([]);
+    const token = localStorage.getItem("token");
 
     const favorisItems = async () => {
-        const token = localStorage.getItem("token");
-        if (token) {
-          try {
-            const response = await fetch(
-              `${import.meta.env.VITE_API_URL}/users/personal-data`,
-              {
-                method: "GET",
-                headers: {
-                  Authorization: `Bearer ${token}`,
-                },
-              }
-            );
-            const data = await response.json();
-            const userId = data.id;
-    
-            const responseFavorites = await fetch(
-              `${import.meta.env.VITE_API_URL}/items/favorites-items/${userId}`,
-              {
-                method: "GET",
-                headers: {
-                  Authorization: `Bearer ${token}`,
-                },
-              }
-            );
-            const favorites = await responseFavorites.json();
-            setItems(favorites);
-          } catch (error) {
-            console.error("Erreur lors de l'appel API:", error);
+      try {
+        const responseFavorites = await fetch(
+          `${import.meta.env.VITE_API_URL}/items/favorites`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
           }
-        }
+        );
+        const favorites = await responseFavorites.json();
+        setItems(favorites);
+      } catch (error) {
+        console.error("Erreur lors de l'appel API:", error);
+      }
     };
-    
+  
     useEffect(() => {
-        favorisItems();
-    });
-
+      favorisItems();
+    }, []);
+  
     return (
         <div>
             <h1 className="text-black text-lg font-bold mb-3 uppercase">Vos annonces favorites</h1>
