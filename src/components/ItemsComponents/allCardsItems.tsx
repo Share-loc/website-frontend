49a9b034e94c9088dcbb0dc5974cd2f8560c32aa
Romain Ramanzin <romain.ramanzin@gmail.com>
2025-03-28 18:32:54 +0100

    fix - message on no result

--- src/components/ItemsComponents/allCardsItems.tsx
+++ src/components/ItemsComponents/allCardsItems.tsx
@@ -13,7 +13,7 @@ const AllCardsItems = ({ items, filter }: AllCardsItemsProps) => {
   return (
     <div className={`grid grid-cols-2 lg:grid-cols-3 ${gridClass} 2xl:grid-cols-4 3xl:grid-cols-5 4xl:grid-cols-6 gap-5 w-full`}>
       {Object.keys(items).length === 0 ? (
-        <p>Vous n'avez pas encore d'annonces.</p>
+        <p>Aucun résultat</p>
       ) : (
         Object.keys(items).map((key) => {
           const item = items[key];
